7865b4a96ba3bfcb48ae1f919956ad57aa7a71e7
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-07 19:40:30 +0530

    Use setValue and patchValue methods.

--- src/app/template-form/temp-form/temp-form.component.ts
+++ src/app/template-form/temp-form/temp-form.component.ts
@@ -7,12 +7,31 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./temp-form.component.scss'],
 })
 export class TempFormComponent {
-  @ViewChild('tempForm') form!: NgForm;
+  @ViewChild('tempForm') tempForm!: NgForm;
 
   public city: string = 'AHM';
   public genders: Array<string> = ['Male', 'Female'];
 
   public onSubmit(): void {
-    console.log(this.form.value);
+    console.log(this.tempForm.value);
+  }
+
+  public onSetValue(): void {
+    this.tempForm.form.setValue({
+      fname: 'John',
+      lname: 'Doe',
+      email: 'johndoe@gmail.com',
+      password: 'John@123',
+      gender: 'Male',
+      location: 'RKT',
+      remember: true,
+    });
+  }
+
+  public onPatchValue(): void {
+    this.tempForm.form.patchValue({
+      fname: 'Peter',
+      lname: 'Parker',
+    });
   }
 }
