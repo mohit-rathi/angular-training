a0e9ec5c6a4a59213d40eb58ed0394b4b290a4ad
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-07 17:22:44 +0530

    Create TemplateFormModule and import FormsModule

---
+++ src/app/template-form/template-form.module.ts
@@ -0,0 +1,10 @@
+// modules
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, FormsModule],
+})
+export class TemplateFormModule {}
