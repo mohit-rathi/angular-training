adc838f3c0e01545106fa1988f31d5b460cfb2d0
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-07 18:11:18 +0530

    Use ngForm and add form controls using ngModel

--- src/app/template-form/temp-form/temp-form.component.ts
+++ src/app/template-form/temp-form/temp-form.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-temp-form',
   templateUrl: './temp-form.component.html',
-  styleUrls: ['./temp-form.component.scss']
+  styleUrls: ['./temp-form.component.scss'],
 })
 export class TempFormComponent {
-
+  public onSubmit(form: NgForm): void {
+    console.log(form);
+  }
 }
