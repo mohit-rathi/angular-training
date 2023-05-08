720e4f2642d8fd43c9cbdb701ebcc0ba41fef9a8
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-08 15:03:14 +0530

    Navigate programatically using absolute path.

--- src/app/routing-demo/route-home/route-home.component.ts
+++ src/app/routing-demo/route-home/route-home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-route-home',
   templateUrl: './route-home.component.html',
-  styleUrls: ['./route-home.component.scss']
+  styleUrls: ['./route-home.component.scss'],
 })
 export class RouteHomeComponent {
+  constructor(private router: Router) {}
 
+  public goToProducts(): void {
+    this.router.navigate(['/products']);
+  }
 }
--- src/app/routing-demo/route-product/route-product.component.ts
+++ src/app/routing-demo/route-product/route-product.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-route-product',
   templateUrl: './route-product.component.html',
-  styleUrls: ['./route-product.component.scss']
+  styleUrls: ['./route-product.component.scss'],
 })
-export class RouteProductComponent {
-
-}
+export class RouteProductComponent {}
