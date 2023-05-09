b0607d5ed1bdd454b4618d800a5ea75652286772
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-09 10:48:35 +0530

    Create form using reactive approach.

--- src/app/reactive-form/react-form/react-form.component.ts
+++ src/app/reactive-form/react-form/react-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-react-form',
   templateUrl: './react-form.component.html',
-  styleUrls: ['./react-form.component.scss']
+  styleUrls: ['./react-form.component.scss'],
 })
-export class ReactFormComponent {
+export class ReactFormComponent implements OnInit {
+  public userForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  public initializeForm(): void {
+    this.userForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null),
+      gender: new FormControl('Male'),
+      location: new FormControl('AHM'),
+      remember: new FormControl(null),
+    });
+  }
+
+  public onSubmit(): void {
+    console.log(this.userForm);
+  }
 }
