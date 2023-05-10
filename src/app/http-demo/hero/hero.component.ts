fdef2b433dc4b5e6baeaf911a36596649e2dc581
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-10 14:53:48 +0530

    Import HttpClientModule and inject HttpClient in HeroComponent.

--- src/app/http-demo/hero/hero.component.ts
+++ src/app/http-demo/hero/hero.component.ts
@@ -1,10 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent {
-
+  constructor(private http: HttpClient) {}
 }
--- src/app/http-demo/http-demo.module.ts
+++ src/app/http-demo/http-demo.module.ts
@@ -1,13 +1,14 @@
 // modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 // components
 import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [HeroComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   exports: [HeroComponent],
 })
 export class HttpDemoModule {}
