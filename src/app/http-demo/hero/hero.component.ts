7ed2b87409e0e85e2c5c0a4c7aafe4f836d52ca6
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-10 15:44:51 +0530

    Http POST request.

--- src/app/http-demo/hero/hero.component.ts
+++ src/app/http-demo/hero/hero.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { environment as env } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-hero',
@@ -8,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
   constructor(private http: HttpClient) {}
+
+  public addHero(heroForm: NgForm): void {
+    const hero = heroForm.value;
+    this.http.post(env.baseUrl + 'heroes.json', hero).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
--- src/app/http-demo/http-demo.module.ts
+++ src/app/http-demo/http-demo.module.ts
@@ -1,6 +1,7 @@
 // modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 // components
@@ -8,7 +9,7 @@ import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [HeroComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   exports: [HeroComponent],
 })
 export class HttpDemoModule {}
