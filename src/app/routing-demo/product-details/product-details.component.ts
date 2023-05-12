354f13c5f378adf32db7110f7a3a45593d375f1c
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-12 11:39:29 +0530

    Unsubscribe params observable on component destroy.

--- src/app/routing-demo/product-details/product-details.component.ts
+++ src/app/routing-demo/product-details/product-details.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
   public productId!: number;
   public productName!: string;
+  public paramsSubscription!: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
@@ -16,12 +18,16 @@ export class ProductDetailsComponent implements OnInit {
     this.productId = parseInt(this.activatedRoute.snapshot.params['id']);
     this.productName = this.activatedRoute.snapshot.params['productName'];
 
-    this.activatedRoute.params.subscribe({
+    this.paramsSubscription = this.activatedRoute.params.subscribe({
       next: (parameters: Params) => {
         console.log(parameters);
         this.productId = parseInt(parameters['id']);
         this.productName = parameters['productName'];
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 }
