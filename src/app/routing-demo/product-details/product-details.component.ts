0c3aa6db2349593fbd67cb61c5dfe269e0b0fde5
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-12 11:16:20 +0530

    Fetch parameters using ActivatedRoute Snapshot.

--- src/app/routing-demo/product-details/product-details.component.ts
+++ src/app/routing-demo/product-details/product-details.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  public productId!: number;
+  public productName!: string;
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.productId = this.activatedRoute.snapshot.params['id'];
+    this.productName = this.activatedRoute.snapshot.params['productName'];
+  }
 }
