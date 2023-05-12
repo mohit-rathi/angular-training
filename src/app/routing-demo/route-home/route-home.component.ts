a18154990d5748507261071bc5e5acbb091391f5
mohit-rathi <mohit.rathi@bacancy.com>
2023-05-12 14:49:00 +0530

    Add login and logout functionality.

--- src/app/routing-demo/route-home/route-home.component.ts
+++ src/app/routing-demo/route-home/route-home.component.ts
@@ -1,25 +1,32 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+// services
+import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-route-home',
   templateUrl: './route-home.component.html',
   styleUrls: ['./route-home.component.scss'],
 })
 export class RouteHomeComponent {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private _router: Router, private _authService: AuthService) {}
 
   public goToProducts(): void {
-    this.router.navigate(['/products']);
+    this._router.navigate(['/products']);
   }
 
   public goToProductDetailsWithQueryParamsAndFragment(): void {
-    this.router.navigate(['/products', '100', 'Laptop'], {
+    this._router.navigate(['/products', '100', 'Laptop'], {
       queryParams: {
         isProductDetails: true,
         isAllowedEditing: false,
       },
       fragment: 'loading',
     });
+  }
+
+  public onLogin() {
+    this._authService.login();
   }
 }
--- src/app/routing-demo/route-product/route-product.component.ts
+++ src/app/routing-demo/route-product/route-product.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+// services
+import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-route-product',
   templateUrl: './route-product.component.html',
   styleUrls: ['./route-product.component.scss'],
 })
 export class RouteProductComponent {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _authService: AuthService
+  ) {}
 
   public reload() {
-    this.router.navigate(['products'], { relativeTo: this.activatedRoute });
+    this._router.navigate(['products'], { relativeTo: this._activatedRoute });
+  }
+
+  public onLogout() {
+    this._authService.logout();
   }
 }
